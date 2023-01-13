42189adeba1dcdf469116193976a0363b88fbb98
oOBoomberOo <boomberisalreadytaken@gmail.com>
2023-01-13 22:17:41 +0900

    Initialized project

---
+++ src/main/kotlin/me/boomber/zetalib/ZetaLib.kt
@@ -0,0 +1,16 @@
+package me.boomber.zetalib
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.ModInitializer
+
+object ZetaLib : ModInitializer, ClientModInitializer {
+    val MOD_ID = "zeta"
+
+    override fun onInitialize() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInitializeClient() {
+        TODO("Not yet implemented")
+    }
+}
