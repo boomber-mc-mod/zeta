529cd1317349880f7e3d908f72c909d6d0f2453c
oOBoomberOo <boomberisalreadytaken@gmail.com>
2023-01-17 21:28:44 +0900

    added logger

--- src/main/kotlin/me/boomber/zetalib/Zeta.kt
+++ src/main/kotlin/me/boomber/zetalib/Zeta.kt
@@ -1,24 +1,22 @@
 package me.boomber.zetalib
 
-import me.boomber.zetalib.client.area.AreaDetectorRenderer
-import me.boomber.zetalib.commands.Commands
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
 import net.minecraft.util.Identifier
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 object Zeta : ModInitializer, ClientModInitializer {
-    const val MOD_ID = "zeta"
+    const val MOD_ID: String = "zeta"
+    val logger: Logger = LoggerFactory.getLogger(MOD_ID)
 
     infix fun id(path: String): Identifier = Identifier(MOD_ID, path)
 
     override fun onInitialize() {
-        TODO("Not yet implemented")
+        logger.info("Initializing zeta-lib")
     }
 
     override fun onInitializeClient() {
-        TODO("Not yet implemented")
+        logger.info("Initializing zeta-lib client")
     }
 }
