0c0fd4318cd03c7904c78676a5b6b324f9045dd0
oOBoomberOo <github@boomber.xyz>
2023-05-11 23:20:10 +0900

    Added Duration.inTicks

--- src/main/kotlin/me/boomber/zetalib/api/math/MathHelper.kt
+++ src/main/kotlin/me/boomber/zetalib/api/math/MathHelper.kt
@@ -1,9 +1,12 @@
 package me.boomber.zetalib.api.math
 
 import net.minecraft.util.math.Vec2f
+import kotlin.time.Duration
 
 operator fun Vec2f.component1() = x
 operator fun Vec2f.component2() = y
 
 fun <T : Comparable<T>> clamp(value: T, min: T, max: T) =
     if (value < min) min else if (value > max) max else value
+
+val Duration.inTicks get() = (inWholeMilliseconds / 50).toInt()
